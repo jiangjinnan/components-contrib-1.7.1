38a639bbfd95e6ca85b69cc5153465a58caec588
Jinnan-PC/jinnan_win10 <jjn47239@ly.com>
2022-06-29 16:42:30 +0800

    revise name resolution compoment: svcreg

--- nameresolution/svcreg/svcreg.go
+++ nameresolution/svcreg/svcreg.go
@@ -29,45 +29,50 @@ func (resolver *Resolver) Init(metadata nameresolution.Metadata) error {
 	var endpoint, appId, hostAddress string
 	var ok bool
 
+	// Extracts register & resolve endpoint
 	if dic, ok := metadata.Configuration.(map[interface{}]interface{}); ok {
 		endpoint = fmt.Sprintf("%s", dic["endpointAddress"])
+		resolver.registerEndpoint = fmt.Sprintf("%s/register", endpoint)
+		resolver.resolveEndpoint = fmt.Sprintf("%s/resolve", endpoint)
+	}
+	if endpoint == "" {
+		return errors.New("service registry endpoint is not configured")
 	}
 
-	resolver.logger.Infof("Resolver.Endpoint: %s", endpoint)
-
+	// Extracts AppID, HostAddress and Port
 	props := metadata.Properties
-
-	resolver.registerEndpoint = fmt.Sprintf("%s/register", endpoint)
-	resolver.resolveEndpoint = fmt.Sprintf("%s/resolve", endpoint)
-
 	if appId, ok = props[nameresolution.AppID]; !ok {
 		return errors.New("AppId does not exist in the name resolution metadata")
 	}
-
 	if hostAddress, ok = props[nameresolution.HostAddress]; !ok {
 		return errors.New("HostAddress does not exist in the name resolution metadata")
 	}
-
 	p, ok := props[nameresolution.DaprPort]
 	if !ok {
 		return errors.New("DaprPort does not exist in the name resolution metadata")
 	}
-
 	port, err := strconv.ParseInt(p, 10, 32)
 	if err != nil {
 		return errors.New("DaprPort is invalid")
 	}
 
+	// Register service (application)
 	var request = RegisterRequest{appId, hostAddress, port}
 	payload, err := json.Marshal(request)
 	if err != nil {
 		return errors.New("fail to marshal register request")
 	}
 	_, err = http.Post(resolver.registerEndpoint, "application/json", bytes.NewBuffer(payload))
+
+	if err == nil {
+		resolver.logger.Infof("App '%s (%s:%d)' is successfully registered.", request.Id, request.HostAddress, request.Port)
+	}
 	return err
 }
 
 func (resolver *Resolver) ResolveID(req nameresolution.ResolveRequest) (string, error) {
+
+	// Invoke resolve service and get resolved target app's endpoint ("{ip}:{port}")
 	payload, err := json.Marshal(req)
 	if err != nil {
 		return "", err
@@ -81,11 +86,11 @@ func (resolver *Resolver) ResolveID(req nameresolution.ResolveRequest) (string, 
 	if err != nil {
 		return "", err
 	}
-
 	return string(result), nil
 }
 
 func NewResolver(logger logger.Logger) *Resolver {
-
-	return &Resolver{logger, "", ""}
+	return &Resolver{
+		logger: logger,
+	}
 }
